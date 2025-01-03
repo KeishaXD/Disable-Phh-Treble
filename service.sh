2444f57305debf0dd5388f9f903ec4ee4a965d3c
Jovallin Keisha Rafael Dewantara <blackopolotong@gmail.com>
2025-01-03 12:36:38 +0700

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+# shellcheck disable=SC2086
+pm disable me.phh.treble.app/.TopLevelSettingsActivity
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+# shellcheck disable=SC2086
+pm enable me.phh.treble.app/.TopLevelSettingsActivity
